46da6bc5f1b3eff07d3d139adee694512f1183b6
JhonathanPerea <jhonathan.perea@peaku.ai>
2023-05-10 15:26:43 -0500

    feat: modification front7 layouts back crud edit

---
+++ app/Http/Controllers/EvidenceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\evidence;
+use Illuminate\Http\Request;
+
+class EvidenceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\evidence  $evidence
+     * @return \Illuminate\Http\Response
+     */
+    public function show(evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\evidence  $evidence
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\evidence  $evidence
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, evidence $evidence)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\evidence  $evidence
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(evidence $evidence)
+    {
+        //
+    }
+}
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -1,17 +1,4 @@
-<!--
-=========================================================
-* Material Dashboard 2 - v3.1.0
-=========================================================
 
-* Product Page: https://www.creative-tim.com/product/material-dashboard
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
 <!DOCTYPE html>
 <html lang="en">
 
@@ -218,93 +205,8 @@
         </div>
     </nav>
     <!-- End Navbar -->
-    <div class="container-fluid py-4">
-        <div class="row">
-            <div class="col-12">
-                <div class="card my-4">
-                    <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                        <div class="bg-gradient-success shadow-success border-radius-lg pt-4 pb-3">
-                            <h6 class="text-white text-capitalize ps-3">Profesores</h6>
-                        </div>
-                    </div>
-                    <div class="card-body px-0 pb-2">
-                        <div class="table-responsive p-0">
-                            <table class="table align-items-center mb-0">
-                                <thead>
-                                <tr>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">ID</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Nombre</th>
-                                    <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Correo</th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Acciones</th>
+    @yield('navbar')
 
-                                </tr>
-                                </thead>
-                                
-                                    <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="d-flex px-2 py-1">
-
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{$user->id}}</h6>
-
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="d-flex px-2 py-4">
-
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{$user->name}}</h6>
-
-                                                </div>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="d-flex px-4 py-2">
-
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">{{$user->email}}</h6>
-
-                                                </div>
-                                            </div>
-                                        </td>
-
-
-
-                                        <td>
-                                            <div class="d-flex px-4 py-2">
-                                                <div class="d-flex flex-column justify-content-cente">
-                                                 
-
-                                                   
-                                                
-                                                    <form action="{{route('users.destroy',$user)}}" method="POST">
-                                                        @method('DELETE')
-                                                        @csrf
-                                                        <input type="submit" class="btn btn-danger" value="Eliminar">
-                                                        <a type="submit" class="btn btn-primary mx-2" value="editar" href="{{route('users.edit',$user)}}">Editar</a>
-                                                    </form>
-                                                 
-                                                </div>
-                                            </div>
-                        </div>
-                        </td>
-
-
-                        </tr>
-                        </tbody>
-                        
-                        </table>
-
-                        <table class="table align-items-center justify-content-center mb-0">
-
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <footer class="footer py-4  ">
         <div class="container-fluid">
             <div class="row align-items-center justify-content-lg-between">
@@ -340,8 +242,6 @@
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
 <script src="{{asset('js/material-dashboard.min.js?v=3.1.0')}}"></script>
-
-@yield('contenido')
 </body>
 
 </html>
