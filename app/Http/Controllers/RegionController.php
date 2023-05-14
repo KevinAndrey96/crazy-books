ee907222602e53954d25714b2b55a899d56b6668
JhonathanPerea <jhonathan.perea@peaku.ai>
2023-05-14 07:02:46 -0500

    feat: modification front11 books controller modification4

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -15,7 +15,7 @@ class RegionController extends Controller
     public function index()
     {
         
-        
+        $region = Region::all();
 
         return view('regions.index',compact('regions'));
     }
