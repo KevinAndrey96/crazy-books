c95a59b0eca478bad11785b0c8496bc11d7139b4
JhonathanPerea <jhonathan.perea@peaku.ai>
2023-05-19 11:37:01 -0500

    feat: modification front experiences regions, books. 7

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -9,7 +9,7 @@
             <div class="card my-4">
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-success shadow-success border-radius-lg pt-4 pb-3">
-                        <h6 class="text-white text-capitalize ps-3">Regiones</h6>
+                        <h6 class="text-white text-capitalize ps-3">Libros</h6>
                     </div>
                 </div>
                 <div class="card-body px-0 pb-2">
@@ -47,7 +47,7 @@
                                 </td>
 
                                 <td class="btn btn-group">
-                                    <div class="d-flex px-4 py-1">
+                                    <div class="d-flex px-4 py-2">
 
                                         <div class="d-flex justify-content-cente">
                                          
