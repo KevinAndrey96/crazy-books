11d2c1efdedf765606c2f3253c00cd4e3b0f37e7
JhonathanPerea <jhonathan.perea@peaku.ai>
2023-05-26 15:40:16 -0500

    EXPO 7.3

--- resources/views/evidences/index.blade.php
+++ resources/views/evidences/index.blade.php
@@ -9,20 +9,25 @@
   <tbody>
     @forelse ($evidences as $evidence)
       <tr>
-        <td class="p-4">
+        <td >
           @if ($evidence->user_id === Auth::id())
             @if (!is_null($evidence->attachment_media_url))
-            <div class="card" style="max-width: 100%; min-width: 100%;">
+            <div class=" card" style="max-width: 100%; min-width: 100%;">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-warning shadow-primary border-radius-lg pt-4 pb-3">
                   <h6 class="text-white text-capitalize ps-3"></h6>
                   <p class="mb-0 text-white ps-3">
-                    <h4 style="margin: 2%">{{$evidence->user->name}}</h4>
+                    <h4 style="text-align: center;">{{$evidence->user->name}}</h4>
                   </p>
                 </div>
-                <img style="width: 80%; margin-top:7%;" class="img-fluid" src="https://crazybooks.com.co{{$evidence->attachment_media_url}}">
+                
+              
+                <img style="width: 50%; margin-top:2%; margin-left:25%;" class="img-flui" src="https://crazybooks.com.co{{$evidence->attachment_media_url}}">
+              
+              
+
               </div>
-              <div class="card-body p-3">
+              <div class="card-body p-3 mx-auto">
                 <h4>Estudiante: {{$evidence->student_name}}</h4>
                 <h4>Curso: {{$evidence->class_room}}</h4>
                 <!-- Botones de editar y eliminar solo para el usuario actual -->
