3f3dca5024a7443368c9476f7ca942aa7d70f3fe
JhonathanPerea <jhonathan.perea@peaku.ai>
2023-07-31 18:44:16 -0500

    cambio pendientes de vista movil79

--- resources/views/regions/show.blade.php
+++ resources/views/regions/show.blade.php
@@ -55,7 +55,7 @@
       <!-- Masthead-->
       <header class="masthead" style="background-image: url('https://i.pinimg.com/originals/d6/0e/c1/d60ec1a1b6b8b6e17dc2d014c3e1cc09.jpg')">
          <div class="container-fluid">
-            <div class="masthead-heading text-uppercase" style="color: #800080; text-shadow: 2px 2px 2px #000000;">{{ $region->name }}</div>
+            <div class="container-fluid masthead-heading text-uppercase" style="color: #800080; text-shadow: 2px 2px 2px #000000;">{{ $region->name }}</div>
             <a class="btn btn-primary btn-xl text-uppercase" href="https://crazybooks.com.co" style="background-color: #5a3fc0; color: #fff;">Volver al mapa interactivo</a>
          </div>
       </header>
